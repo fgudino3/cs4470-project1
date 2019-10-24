70e8515d05c61b511773fb92139a2331aa325cb4
Franky Gudino <frankyjg96@gmail.com>
2019-10-24 09:27:21 -0700

    added output.txt

--- project1/src/main/Project.java
+++ project1/src/main/Project.java
@@ -21,95 +21,92 @@ public class Project
         
         while (trace < 0)
         {
-        	trace = 3;
-//            System.out.print("Enter trace level (>= 0): [0] ");
-//            try
-//            {
-//                buffer = stdIn.readLine();
-//            }
-//            catch (IOException ioe)
-//            {
-//                System.out.println("IOError reading your input!");
-//                System.exit(1);
-//            }
-//            
-//            if (buffer.equals(""))
-//            {
-//                trace = 0;
-//            }
-//            else
-//            {            
-//                try
-//                {
-//                    trace = Integer.parseInt(buffer);
-//                }
-//                catch (NumberFormatException nfe)
-//                {
-//                    trace = -1;
-//                }
-//            }
+            System.out.print("Enter trace level (>= 0): [0] ");
+            try
+            {
+                buffer = stdIn.readLine();
+            }
+            catch (IOException ioe)
+            {
+                System.out.println("IOError reading your input!");
+                System.exit(1);
+            }
+            
+            if (buffer.equals(""))
+            {
+                trace = 0;
+            }
+            else
+            {            
+                try
+                {
+                    trace = Integer.parseInt(buffer);
+                }
+                catch (NumberFormatException nfe)
+                {
+                    trace = -1;
+                }
+            }
         }
 
         while ((hasLinkChange < 0) || (hasLinkChange > 1))
         {
-        	hasLinkChange = 0;
-//            System.out.print("Will the link change (1 = Yes, 0 = No): [0] ");
-//            try
-//            {
-//                buffer = stdIn.readLine();
-//            }
-//            catch (IOException ioe)
-//            {
-//                System.out.println("IOError reading your input!");
-//                System.exit(1);
-//            }
-//            
-//            if (buffer.equals(""))
-//            {
-//                hasLinkChange = 0;
-//            }
-//            else
-//            {            
-//                try
-//                {
-//                    hasLinkChange = Integer.parseInt(buffer);
-//                }
-//                catch (NumberFormatException nfe)
-//                {
-//                    hasLinkChange = -1;
-//                }
-//            }
+            System.out.print("Will the link change (1 = Yes, 0 = No): [0] ");
+            try
+            {
+                buffer = stdIn.readLine();
+            }
+            catch (IOException ioe)
+            {
+                System.out.println("IOError reading your input!");
+                System.exit(1);
+            }
+            
+            if (buffer.equals(""))
+            {
+                hasLinkChange = 0;
+            }
+            else
+            {            
+                try
+                {
+                    hasLinkChange = Integer.parseInt(buffer);
+                }
+                catch (NumberFormatException nfe)
+                {
+                    hasLinkChange = -1;
+                }
+            }
         }
 
         while (seed < 1)
         {
-        	seed = 195;
-//            System.out.print("Enter random seed: [random] ");
-//            try
-//            {
-//                buffer = stdIn.readLine();
-//            }
-//            catch (IOException ioe)
-//            {
-//                System.out.println("IOError reading your input!");
-//                System.exit(1);
-//            }
-//            
-//            if (buffer.equals(""))
-//            {
-//                seed = System.currentTimeMillis();
-//            }
-//            else
-//            {            
-//                try
-//                {
-//                    seed = (Long.valueOf(buffer)).longValue();
-//                }
-//                catch (NumberFormatException nfe)
-//                {
-//                    seed = -1;
-//                }
-//            }
+            System.out.print("Enter random seed: [random] ");
+            try
+            {
+                buffer = stdIn.readLine();
+            }
+            catch (IOException ioe)
+            {
+                System.out.println("IOError reading your input!");
+                System.exit(1);
+            }
+            
+            if (buffer.equals(""))
+            {
+                seed = System.currentTimeMillis();
+            }
+            else
+            {            
+                try
+                {
+                    seed = (Long.valueOf(buffer)).longValue();
+                }
+                catch (NumberFormatException nfe)
+                {
+                    seed = -1;
+                }
+            }
         }
 
         if (hasLinkChange == 0)
